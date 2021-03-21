568885818f9ef29180fbcf75c25f378267a9bd87
paulanguedes <paulanguedes@gmail.com>
2021-03-21 18:04:25 -0300

    first steps of the sign in page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../SignIn/index';
-import SignUp from '../SignUp/index';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
 
